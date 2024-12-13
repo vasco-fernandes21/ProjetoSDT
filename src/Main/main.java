d0cd0e938012696da8c355382e3ff4572aafd3c6
Vasco Fernandes <vascocfernandes21@gmail.com>
2024-12-13 00:08:05 +0000

    sprint 6 concluído

---
+++ src/Main/main.java
@@ -0,0 +1,19 @@
+package System;
+
+import System.Elemento;
+
+public class Main {
+    public static void main(String[] args) {
+        // Inicia três elementos
+        Elemento elemento1 = new Elemento();
+        Elemento elemento2 = new Elemento();
+        Elemento elemento3 = new Elemento();
+
+        // Adiciona os elementos ao mapa de receivers
+        Elemento.getReceiverMap().put(elemento1.getUuid(), elemento1.getReceiver());
+        Elemento.getReceiverMap().put(elemento2.getUuid(), elemento2.getReceiver());
+        Elemento.getReceiverMap().put(elemento3.getUuid(), elemento3.getReceiver());
+
+        System.out.println("Três elementos foram iniciados.");
+    }
+}
